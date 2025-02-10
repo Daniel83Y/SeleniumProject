072f1a2e39605017c35f5dd6f214a0467f4c63f2
Daniel <43648186+Daniel83Y@users.noreply.github.com>
2025-02-10 18:57:01 +0200

    Second Commit

---
+++ Selenium Project/SmartBear_Product_Page.py
@@ -0,0 +1,37 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+class BearStoreProductPage:
+    def __init__(self,driver:webdriver.Edge):
+        """Initialize the BearStoreMainPage with a WebDriver instance."""
+        self.driver = driver
+
+    def breadcrumbs(self):
+        """Returns the breadcrumbs trail"""
+        return self.driver.find_element(By.CLASS_NAME,'breadcrumb-container')
+
+    def current_active_crumb(self):
+        return self.breadcrumbs().find_element(By.CLASS_NAME,'active')
+
+    def inactive_breadcrumbs(self):
+        return self.breadcrumbs().find_elements(By.CSS_SELECTOR,'ol > li.breadcrumb-item > a')
+
+    def breadcrumbs_nav(self,content):
+        crumbs_list = self.inactive_breadcrumbs()
+        for crumb in crumbs_list:
+            if crumb.text == content:
+                crumb.click()
+
+    def get_product_name(self):
+        """Returns the product name"""
+        return self.driver.find_element(By.CSS_SELECTOR, 'div.page-title > h1')
+
+    def get_product_price(self):
+        """Returns the product price"""
+        return self.driver.find_element((By.CSS_SELECTOR,'div.pd-price > span'))
+    def add_to_cart(self):
+        self.driver.find_element(By.CLASS_NAME,"btn-add-to-cart").click()
+    def quantity(self):
+        return self.driver .find_element(By.CSS_SELECTOR,"div>input")
+    def change_quantity(self,random_quantity):
+        self.quantity().clear()
+        self.quantity().send_keys(random_quantity)
